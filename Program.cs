f75bba53875abb584dac5a14273ce56e657cdc19
Christian Simonsen <crsdev@gmail.com>
2024-03-11 14:31:12 +0100

    Step 3 Reading and Saving credentials v1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿const string CONFIG_FILE = ".config";
+string pantry_id = "";
+
+if(File.Exists(CONFIG_FILE)){
+    string[] configDetails = File.ReadAllLines(CONFIG_FILE);
+    pantry_id = configDetails[0];
+} else{
+    Console.Write("Input Pantry Id : ");
+    pantry_id = Console.ReadLine() ?? "";
+    if(pantry_id.Length > 0){
+        File.WriteAllText(CONFIG_FILE, pantry_id);
+    }
+}
